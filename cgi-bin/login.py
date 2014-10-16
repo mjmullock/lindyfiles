edb4755b189e663ebf90e06802fe3dd990433e41
Mark Mullock <mmullock@u.rochester.edu>
2014-10-16 17:19:05 -0400

    Testing cookies

--- cgi-bin/login.py
+++ cgi-bin/login.py
@@ -4,11 +4,14 @@ import os
 import cgi
 import cgitb
 import sqlite3
+import Cookie
 
-def build_html_page(content_line):
+def build_html_page(content_line, cookie=None):
 	s = ''
-	s += ("Content-Type: text/html\n\n")
-	s += ("<html>")
+	s += ("Content-Type: text/html\n")
+    if cookie is not None:
+        print cookie
+	s += ("\n<html>")
 	s += ("<body>")
 	s += ("<p>" + content_line + "\n")
 	s += ("</body>")
@@ -17,15 +20,24 @@ def build_html_page(content_line):
 
 cgitb.enable()
 
-import Cookie
+cookie_string = os.environ.get('HTTP_COOKIE')
+if cookie_string:
+    c = Cookie.SimpleCookie(cookie_string)
+    print build_html_page("I have your cookie!")
+    exit(0)
+else:
+    c = Cookie.SimpleCookie()
+    c['name'] = 'test_cookie'
+    print build_html_page("Building cookie...", cookie=c)
+    exit(0)
+    
 
 form = cgi.FieldStorage()
 name = form["name"].value
 password = form['password'].value
 signup_type = form['signup_type'].value
 if signup_type not in ['register', 'login']:
-	page = build_html_page("Signup type incorrectly specified.")
-	print page
+	print build_html_page("Signup type incorrectly specified.")
 	exit(0)
 
 
