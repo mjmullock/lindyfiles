cf4a99c1a87f5b41f96c016d8d26e59603d92c06
Mark Mullock <mmullock@u.rochester.edu>
2014-11-08 18:24:48 -0500

    Adding features to message board (2)

--- cgi-bin/load_messages.py
+++ cgi-bin/load_messages.py
@@ -3,12 +3,37 @@
 import cgi
 import cgitb
 import os
+import sqlite3
+import Cookie
+from datetime import datetime
 
 cgitb.enable()
 form = cgi.FieldStorage()
 new_message = form['stuff'].value
+conn = sqlite3.connect'/home2/mmullock/public_html/lindyfiles/lindyfiles.db')
+cur = conn.cursor()
+
+def do_err():
+	print "Content-type: text/html"
+	print
+	print "User not recognized. Only logged-in users can view the message board."
+	exit(1)
+
+cookie_string = os.environ.get('HTTP_COOKIE')
+if not cookie_string:
+	do_err()
+
+try:
+	ck = Cookie.SimpleCookie(cookie_string)
+	sessid = ck['sessid'].value
+	cur.execute("SELECT email FROM users WHERE sessid = ?", (sessid,))
+	results = cur.fetchone()
+	email = results[0]
+except:
+	do_err()
 
 with open("message_board.txt", "a") as f:
+	msg = str( (new_message, email, datetime.now()) )
 	f.write(new_message)
 	f.write("\n")
 	f.close()
