e31813f2638c7715a43558c67bd9537be8463cf6
Mark Mullock <mmullock@u.rochester.edu>
2014-11-08 17:37:28 -0500

    Trying JavaScript (2)

--- cgi-bin/load_messages.py
+++ cgi-bin/load_messages.py
@@ -4,18 +4,8 @@ import cgi
 import cgitb
 import os
 
-def make_page():
-	s = "Content-Type: text/html\n\n"
-	s += '<html>\n'
-	s += '<body>\n'
-	
-	with open("message_board.txt", "r+") as f:
-		for line in f:
-			s += line + '\n'
-
-	s += '</body>\n'
-	s += '</html>'
-	print s
-
 cgitb.enable()
-make_page()
+
+with open("message_board.txt", "r+") as f:
+	for line in f:
+		s += line + '\n'
