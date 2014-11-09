c23ef6d324e48dd2ebcf9153b69fa71c5d799453
Mark Mullock <mmullock@u.rochester.edu>
2014-11-08 19:57:16 -0500

    Fixing cookie behavior (4)

--- cgi-bin/login.py
+++ cgi-bin/login.py
@@ -44,7 +44,8 @@ if not len(res):
 	 	cookie = Cookie.SimpleCookie()
 	 	cookie['sessid'] = sessid
 		expiration = datetime.datetime.now() + datetime.timedelta(days=30)
-		cookie['sessid']['expires'] = expiration.strftime("%a, %d %b %Y %H:%M:%S GMT")
+		cookie['sessid']['expires'] = 'Sun, 23 Nov 2014 00:00:01 GMT'
+		#cookie['sessid']['expires'] = expiration.strftime("%a, %d %b %Y %H:%M:%S GMT")
 	 	page = build_html_page("Welcome!", cookie)
 #		page = build_html_page("Welcome, " + name)
 	else:
