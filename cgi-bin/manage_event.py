f74816fd62ce5e160d19a07b15af862fb3689f2c
Mark Mullock <mmullock@u.rochester.edu>
2014-11-18 11:13:47 -0500

    Minor changes to event display (2)

---
+++ cgi-bin/manage_event.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import cgi
+import cgitb
+import sqlite3
+import os
+
+def main():
+	attendees_implemented = 0
+	
+	cgitb.enable()
+	cookie = Cookie.SimpleCookie(os.environ.get("HTTP_COOKIE"))
+	conn = sqlite3.connect('/home2/mmullock/public_html/lindyfiles/lindyfiles.db')
+	cur = conn.cursor()
+	form = cgi.FieldStorage()
+	eventID = form['eventID'].value
+	
+	if attendees_implemented:
+		cur.execute("select attendees from events where id=?", (eventID,))
+		res = str(cur.fetchone())
+	else:
+		res = ""
+		
+	sessid = cookie["sessid"]
+	cur.execute("select email from users where sessid=?", (sessid,))
+	email = cur.fetchone()
+	res += str(email)
+	if attendees_implemented:
+		cur.execute("update events set attendees=? where id=?", (res, eventID))
+	
+	cur.close()
+	conn.close()
+	print "Content-type: text\html"
+	print
+	print res
+	
+if __name__ == "__main__":
+	main()
