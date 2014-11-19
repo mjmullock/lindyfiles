a1004f188ec9881654d29a2eb5dbe6a88ccb2aca
Mark Mullock <mmullock@u.rochester.edu>
2014-11-18 18:35:54 -0700

    Committing to push

--- cgi-bin/queries.py
+++ cgi-bin/queries.py
@@ -4,53 +4,73 @@ from math import radians, sqrt, sin, cos, atan2
 
 # geographic distance function from 
 # http://stackoverflow.com/questions/8858838/need-help-calculating-geographical-distance. 
-# returns distance in km
+# returns distance in miles
 def geocalc(lat1, lon1, lat2, lon2):
-    lat1 = radians(lat1)
-    lon1 = radians(lon1)
-    lat2 = radians(lat2)
-    lon2 = radians(lon2)
+	lat1 = radians(lat1)
+	lon1 = radians(lon1)
+	lat2 = radians(lat2)
+	lon2 = radians(lon2)
 
-    dlon = lon1 - lon2
+	dlon = lon1 - lon2
 
-    EARTH_R = 6372.8
+	EARTH_R = 3959 
 
-    y = sqrt(
-        (cos(lat2) * sin(dlon)) ** 2
-        + (cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dlon)) ** 2
-        )
-    x = sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(dlon)
-    c = atan2(y, x)
-    return EARTH_R * c
+	y = sqrt(
+		(cos(lat2) * sin(dlon)) ** 2
+		+ (cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dlon)) ** 2
+		)
+	x = sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(dlon)
+	c = atan2(y, x)
+	return EARTH_R * c
 
 # get nearby events
-def get_nearby_events(city, state):
-    
-    conn = sqlite3.connect('/home2/mmullock/public_html/lindyfiles/lindyfiles.db')
-    cur = conn.cursor()
+def get_nearby_events(city, region):
+	
+	conn = sqlite3.connect('/home2/mmullock/public_html/lindyfiles/lindyfiles.db')
+	cur = conn.cursor()
 
-    cur.execute("SELECT latitude, longitude FROM CITIES WHERE UPPER(city) = UPPER(?) AND UPPER(state) = UPPER(?)", (city, state))
-    res = cur.fetchall()
-    print res            
+	cur.execute("SELECT latitude, longitude FROM CITIES WHERE UPPER(city) = UPPER(?) AND UPPER(region) = UPPER(?)", (city, region))
+	res = cur.fetchall()
 
-    cur.close()
-    conn.close()
+	lats = []
+	longs = []	
+
+	for r in res:
+		lats.append(r[0])
+		longs.append(r[1])
+
+	mean_lat = sum(lats)/float(len(lats))
+	mean_long = sum(longs)/float(len(longs))	
+
+	conn.create_function("geocalc", 4, geocalc)
+
+	# select all events whose location is within 50 miles of the given city
+	query = '''
+			select name, start_date, id
+			from events
+			having 	
+'''	
+
+	cur.close()
+	conn.close()
+
+get_nearby_events('Rochester', 'NY')
 
 # get upcoming events
 def get_upcoming_events():
-    
-    conn = sqlite3.connect('/home2/mmullock/public_html/lindyfiles/lindyfiles.db')
-    cur = conn.cursor()
+	
+	conn = sqlite3.connect('/home2/mmullock/public_html/lindyfiles/lindyfiles.db')
+	cur = conn.cursor()
 
-    format = "%Y-%m-%d"
-    today = datetime.datetime.today()
-    today = today.strftime(format)
-    future = today + datetime.timedelta(days=30)
-    future = future.strftime(format)
-    cur.execute("SELECT * FROM EVENTS WHERE start_date BETWEEN ? AND ?", (today, future))        
-    res = cur.fetchall()
-    print res
+	format = "%Y-%m-%d"
+	today = datetime.datetime.today()
+	today = today.strftime(format)
+	future = today + datetime.timedelta(days=30)
+	future = future.strftime(format)
+	cur.execute("SELECT * FROM EVENTS WHERE start_date BETWEEN ? AND ?", (today, future))		 
+	res = cur.fetchall()
+	print res
 
-    cur.close()
-    conn.close()
+	cur.close()
+	conn.close()
 
